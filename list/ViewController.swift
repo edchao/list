c25cbc15326b0e82f6b7097fb116f3064b829201
Ed Chao <eddchao@gmail.com>
2016-01-02 12:57:01 -0800

    Initial Commit

---
+++ list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  list
+//
+//  Created by Ed Chao on 1/2/16.
+//  Copyright © 2016 Ed Chao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
